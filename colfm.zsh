d4fa10e71a1e149fbfeaaeb6ec6b138202a33b2b
Julian Langschaedel <meta.rb@gmail.com>
2011-03-07 18:17:42 +0100

    add directory var to colfm.zsh

--- colfm.zsh
+++ colfm.zsh
@@ -4,24 +4,30 @@
 # cargs: like xargs with the current marked files
 # csel: list selected files line by line
 
+if [ -r /tmp/$USER/.colfmdir ]; then
+  colfm_dir="/tmp/$USER"
+else
+  colfm_dir="~"
+fi
+
 ccd() {
   # This is nasty, but widgets weren't supposed to run curses apps.
   colfm.rb "$@" <$TTY
   print -n "\033[A"
   zle && zle -I                 # force redrawing of prompt
-  cd "$(cat ~/.colfmdir)"
+  cd "$(cat $colfm_dir/.colfmdir)"
 }
 
 cargs() {
-  xargs -r -0 "$@" <~/.colfmsel
+  xargs -r -0 "$@" <$colfm_dir/.colfmsel
 }
 
 csel() {
   if tty -s; then
-    tr '\0' '\n' <~/.colfmsel
+    tr '\0' '\n' <$colfm_dir/.colfmsel
     echo
   else
-    tr '\n' '\0' |  >~/.colfmsel
+    tr '\n' '\0' |  >$colfm_dir/.colfmsel
   fi
 }
 
